4bc35b7073e4a6385a4b5d65a5342f4369003f3f
Jose Luis Ochoa <joseluisochoag@gmail.com>
2023-04-23 01:08:12 -0500

    Actualizando la Clase 2 y Realizar el Ejercicio Propuesto

--- Clase-2/index.js
+++ Clase-2/index.js
@@ -1,6 +1,7 @@
 /* Tipos de Variables */
 var varNumber = 12;                 // Numero, entero o decimal
-var varString = "hola mundo";       // String/cadena
+var varString1 = "hola mundo.";     // String/cadena
+var varString2 = 'Hola Mundo!';     // String/cadena 
 var varLogico = false;              // Booleano, si no, falso, verdadero
 var varNull = null;                 // nulo, espacio vacio en memoria
 var varIndefi = undefined;          // no definido
@@ -13,3 +14,83 @@ const varObject = {
 }
 
 console.log('Variable numero :>> ', varNumber);
+
+// con la sentencia var la variable es de entorno global
+// diferencia entre let y var
+var prueba = "esta es la var";
+for (let index = 0; index < varArray.length; index++) {
+    var prueba = varArray[index];
+    console.log('prueba :>> ',prueba);
+    
+}
+
+// con la sentencia let la variable pertenece al ambito donde se crea o inicia
+let prueba2 = "esta es la var";
+for (let index = 0; index < varArray.length; index++) {
+    let prueba2 = varArray[index];
+    console.log('prueba2 :>> ',prueba2);
+    
+}
+console.log('prueba2 :>> ', prueba2);
+
+// con el operador typeof podemos saber que tipo de variable es la variable.
+
+console.log('el tipo de variable es la variable myArray  :>> ', typeof varArray);
+console.log('el tipo de variable es la variable prueba2  :>> ', typeof prueba2);
+
+/* notacion
+ ; el punto y coma define el final de la linea
+ . se utliza para acceder a las propiedades de un objeto o function predefinida.
+   la propiedad log en console -> console.log  
+   la propiedad nombre en varObject  ->  varObject.nombre   
+ [] para declarar Array o Listas, o indicar/acceder a la posicion especifica de un array o lista 
+    let myArray = [1,1,2,3,4,5,6]  ->  let valor=myArray[2]
+ {} son utilizados para delimitar funciones o bloques de procedimiento, Objetos, Estructuras de control
+    for (let i=1;i<10;i++) {
+        console.log( i );
+    }
+    const varObject = {
+    nombre: "jose",
+    apellido: "Ochoa",
+    edad: 14
+    }
+ () para definir funciones 
+    function buscar() {
+        return true;
+    }
+ */
+
+// listas
+
+let myArray1 = [1,2,"carro",true];
+let myArray2 = new Array(4);
+let myArray3 = new Array(1,2,"carro",true);
+console.log('myArray1 :>> ', myArray1);
+console.log('myArray2 :>> ', myArray2);
+console.log('myArray3 :>> ', myArray3);
+console.log('la posicion 2 en myArray3 es :>> ', myArray3[2]);
+
+// Objetos
+
+let varObject1 = {
+    nombre: "jose",
+    apellido: "Ochoa",
+    edad: 14,
+    materias: ["matematicas","logica"]
+}
+
+// let varObject2 = new varObject1;
+// varObject2.apellido="perez";
+
+// console.log('apellido en varObject2 :>> ', varObject2.apellido);
+
+// Fechas
+let dias = ["dom","lun","mar","mie","jue","vie","sab"]
+let fecha = new Date();
+console.log('fecha :>> ', fecha);
+let diam = dias[fecha.getDay()];  // devuelve el numero de dia de la semana siendo domingo el 0
+let dia = fecha.getDate();
+let mes = fecha.getMonth()+1;
+let ano = fecha.getFullYear();
+
+console.log('la fecha es :>> ',diam, dia ,mes ,ano );
---
+++ Ejercicio-2/index.js
@@ -0,0 +1,12 @@
+let miLista = ["Jose Luis Ochoa",45,true,new Date(1976, 2, 5),{titulo:"El hombre que calculaba",autor:"Julio César de Mello y Souza",fecha: new Date(1949,6,6),url:"https://es.wikipedia.org/wiki/El_hombre_que_calculaba"}];
+console.log("mi nombre es: ",miLista[0]);
+console.log("mi edad es: ",miLista[1]);
+console.log("Fecha de Nacimiento: ",miLista[3]);
+console.log("soy Desarrollador ? ",miLista[2]);
+console.log("mi libro Favorito es: ");
+console.log("Titulo : ",miLista[4].titulo);
+console.log("Autor  : ",miLista[4].autor);
+console.log("Fecha  : ",miLista[4].fecha);
+console.log("Url    :",miLista[4].url);
+
+
